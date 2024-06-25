ca1ea3a68d3873b716086a3713deedc0fd151c6f
Ozoda017 <135359979+Ozoda017@users.noreply.github.com>
2024-06-25 19:30:40 +0500

    Update Main.java

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -1,9 +1,11 @@
 package homework;
 
+
 public class Main{
     public static void main(String[] args){
-        User user = new User("Bob");
-        user.report();
-        user.save();
+        Teacher s1  = new Teacher(666, "Мария", "Петрова");
+        Teacher teacher = s1;
+        teacher.report();
+        teacher.update();
     }
 }
